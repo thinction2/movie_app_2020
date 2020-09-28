7d5e338d51cd383b70bba96bfe10fd52aa1c2fb5
thinction1 <thinction_a_jin@naver.com>
2020-09-28 18:08:30 +0900

    Updates

---
+++ precache-manifest.ce994af71ff20c4ac0f517c9b0a1d260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9425ef5e04600c9d6ca04bf75017ebe4",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a6e0918a0a344358f78d",
+    "url": "/movie_app_2020/static/css/main.e59d4e01.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e0918a0a344358f78d",
+    "url": "/movie_app_2020/static/js/main.94834513.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
